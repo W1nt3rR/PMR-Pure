7b1aa5d9f83e4c063a1d47cc3591cdccf62bbb83
W1nt3rR <frostywinterr@gmail.com>
2022-06-18 11:07:48 +0200

    trying inline calling of function

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
-let nav = document.getElementById("nav");
-let navButton = document.getElementById("navButton");
+// let nav = document.getElementById("nav");
+// let navButton = document.getElementById("navButton");
 
 
 function navResposnive(){
+    let nav = document.getElementById("nav");
+    
     if(nav.style["max-height"] == "80px") 
         nav.style["max-height"] = "100vh"
     else
         nav.style["max-height"] = "80px";
 }
 
-navButton.addEventListener("click", navResposnive);
+// navButton.addEventListener("click", navResposnive);
