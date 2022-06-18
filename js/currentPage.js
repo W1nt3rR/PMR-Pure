53ba19cd973c8f8187b559b5e71cae5f416f4d32
W1nt3rR <frostywinterr@gmail.com>
2022-06-18 14:13:10 +0200

    cleanup

--- js/currentPage.js
+++
@@ -1,8 +0,0 @@
-function fn() {
-    let page = document.getElementById("page").textContent;
-    let elements = document.getElementsByClassName("navItem");
-    elements[page].style["background-color"] = "#69006969";
-    elements[page].style["color"] = "#960096"; 
-}
-
-setTimeout(fn, 500);
