1acfeb54bf8979995853b9ac75cff62935697539
W1nt3rR <frostywinterr@gmail.com>
2022-06-18 14:01:21 +0200

    console log not needed

--- js/currentPage.js
+++ js/currentPage.js
@@ -1,7 +1,6 @@
 function fn() {
     let page = document.getElementById("page").textContent;
     let elements = document.getElementsByClassName("navItem");
-    console.log(elements);
     elements[page].style["background-color"] = "#69006969";
     elements[page].style["color"] = "#960096"; 
 }
