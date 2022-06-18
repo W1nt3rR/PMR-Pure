f7cde6e2f66e7fac3bc03ec68e01336e12072baf
W1nt3rR <frostywinterr@gmail.com>
2022-06-18 13:39:22 +0200

    create a html variable p tag to get page number

--- js/currentPage.js
+++ js/currentPage.js
@@ -1,6 +1,9 @@
 function fn(page) {
+    let page = document.getElementById("page").textContent;
     let elements = document.getElementsByClassName("navItem");
     console.log(elements);
     elements[page].style["background-color"] = "#96009669";
     elements[page].style["color"] = "#960096"; 
 }
+
+setTimeout(fn, 500);
