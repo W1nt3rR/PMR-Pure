277c3157e1d6992a0fd6b66eabaa48ce44d01502
W1nt3rR <frostywinterr@gmail.com>
2022-06-18 14:05:20 +0200

    testing js navbar only without jquery

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,12 @@
 fetch('js/nav.html')
 .then(res => res.text())
 .then(text => {
-    let oldelem = document.querySelector("script#navbar-js");
-    let newelem = document.createElement("nav");
-    newelem.innerHTML = text;
-    newelem.id = "nav";
-    newelem.style["max-height"] = "80px";
-    oldelem.parentNode.replaceChild(newelem, oldelem);
+    // let oldelem = document.querySelector("script#navbar-js");
+    let nav = document.getElementById("nav");
+    // let newelem = document.createElement("nav");
+    nav.innerHTML = text;
+    // newelem.id = "nav";
+    // newelem.style["max-height"] = "80px";
+    // oldelem.parentNode.replaceChild(newelem, oldelem);
 });
 
